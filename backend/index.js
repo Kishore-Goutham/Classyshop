5f0d612fb0692665061f76252bbc17bd29b5394a
Kishore-Goutham <kishoregoutham123@gmail.com>
2026-03-04 14:40:06 +0530

    updated address

--- backend/index.js
+++ backend/index.js
@@ -18,10 +18,8 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000","http://localhost:3001"]
 }));
-
-app.options("*", cors());
 app.use(express.json())
 app.use(cookieParser());
 app.use(morgan())
