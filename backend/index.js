d301c73c5515d44453d013f805e65d7fce395b17
Kishore-Goutham <kishoregoutham123@gmail.com>
2026-03-04 19:49:29 +0530

    added frontendurl

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:3000","http://localhost:3001"]
+    origin: ["https://classyshop-seven.vercel.app/","http://localhost:3001"]
 }));
 app.use(express.json())
 app.use(cookieParser());
